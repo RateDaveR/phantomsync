452b581f14abd3ad67c4180f178ec2ea372b3ba0
DaveVED <dave.dennis@gs.com>
2025-01-06 23:19:12 -0600

    feat(bootstrap): seems as if I can run now nix-develop, nix-build, etc... so we are able to publish the flake. Pushing so I can test with my home manager.

--- flake.nix
+++ flake.nix
@@ -1,12 +1,10 @@
 {
   description = "👻🌐 PhantomSync";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
     zig-overlay.url = "github:mitchellh/zig-overlay";
   };
-
-  outputs = { self, nixpkgs, zig-overlay }:
+  outputs = { self, nixpkgs, zig-overlay }: 
     let
       # Helper function to create package for each system
       forAllSystems = nixpkgs.lib.genAttrs [
@@ -14,22 +12,13 @@
         "aarch64-linux"
         "riscv64-linux"
       ];
-
-      # Package builder for each system
-      packageFor = system: 
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in pkgs.callPackage ./nix/package.nix {
+    in {
+      packages = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.callPackage ./nix/package.nix {
           zig = zig-overlay.packages.${system}."0.13.0";
         };
-    in {
-      # Packages for each system
-      packages = forAllSystems (system: {
-        default = packageFor system;
-        phantomsync = packageFor system;  # Named package
       });
 
-      # Development shell for each system
       devShells = forAllSystems (system: {
         default = nixpkgs.legacyPackages.${system}.callPackage ./nix/devShell.nix {
           zig = zig-overlay.packages.${system}."0.13.0";
